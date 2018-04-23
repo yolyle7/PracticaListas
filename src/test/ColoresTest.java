78946fe6b97430ba133ca50f581dfe2032daab9e
jsc0197 <jsc1197@gmail.com>
2018-04-23 12:19:32 +0200

    ColoresTest

---
+++ src/test/ColoresTest.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+import modelo.Colores;
+
+class ColoresTest {
+	
+	ArrayList<Colores> repetidos = new ArrayList<>();
+
+
+	@Test
+	void testSortearColor() {
+		repetidos.add(Colores.azul);
+		Colores instancia=new Colores(repetidos);	
+		for (int i = 0; i < 100; i++) {
+			assertNotEquals("azul", instancia[i]);
+		}
+	}
+}
+//Arraylist repetidos;
+//repetidos.add(colores.azul);
+//instancia.sortearcolor(repetidos);
+//for i=100;
+//nunca salga el azul. assertEquals
