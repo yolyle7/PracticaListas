1b30dc184dc28a55fad02361b7830249bc49cb5f
Macolbar <elgithubdemacolbar@gmail.com>
2018-04-23 12:15:30 +0200

    codigo color/fila

--- src/modelo/Lista.java
+++ src/modelo/Lista.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Lista implements Listable{
-
+	
 	private LinkedList<Colores> lista;
 	
 	public Lista(){
@@ -16,34 +16,53 @@ public class Lista implements Listable{
 		return lista;
 	}
 
-
 	public void setLista(LinkedList<Colores> lista) {
 		this.lista = lista;
 	}
 
-
 	@Override
 	public void addLista(Colores color) {
-		// TODO Auto-generated method stub
+		lista.add(color);
 		
 	}
-
+	
+	
 	@Override
 	public int borrarColores(ArrayList<Integer> posiciones) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		for (int i = 0; i < posiciones.size(); i++) {
+			this.lista.remove(posiciones.get(i));
+		}
+		return posiciones.size();
 	}
 
 	@Override
 	public ArrayList<Integer> recorrerAutomatico() {
-		// TODO Auto-generated method stub
-		return null;
+		 ArrayList<Integer> posiciones = new  ArrayList<Integer>();
+		
+		for (int i = 0; i < lista.size()-2; i++) {
+			if((lista.get(i).equals(lista.get(i+1))) && ((lista.get(i+1))).equals(lista.get(i+2))){
+				posiciones.add(i);
+				posiciones.add(i+1);
+				posiciones.add(i+2);
+			}
+		}
+		return posiciones;
 	}
 
 	@Override
 	public ArrayList<Integer> busquedaColor(Colores color) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Integer> posiciones = new ArrayList<Integer>();
+		
+		for (int i = 0; i < lista.size(); i++) {
+			if(lista.contains(color)){
+				posiciones.add(i);
+			}
+		}
+		return posiciones;
 	}
+
+	
+
 
 }
