22c91d4c48bf3c3cd3ca03b38007c95873fd4b1a
Linazzy <36626979+Linazzy@users.noreply.github.com>
2018-04-24 09:18:43 +0200

    metodos de lista finiquitados

--- src/modelo/Lista.java
+++ src/modelo/Lista.java
@@ -1,73 +1,3 @@
-<<<<<<< HEAD
-package modelo;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-public class Lista implements Listable{
-	
-	private LinkedList<Colores> lista;
-	
-	public Lista(){
-		super();
-		this.lista=new LinkedList<Colores>();
-	}
-	
-	
-	public LinkedList<Colores> getLista() {
-		return lista;
-	}
-
-	public void setLista(LinkedList<Colores> lista) {
-		this.lista = lista;
-	}
-
-	@Override
-	public void addLista(Colores color) {
-		this.lista.add(color);
-	}
-	
-	
-	@Override
-	public int borrarColores(ArrayList<Integer> posiciones) {
-		
-		for (int i = 0; i < posiciones.size(); i++) {
-			this.lista.remove(posiciones.get(i));
-		}
-		return posiciones.size();
-	}
-
-	@Override
-	public ArrayList<Integer> recorrerAutomatico() {
-		 ArrayList<Integer> posiciones = new  ArrayList<Integer>();
-		
-		for (int i = 0; i < lista.size()-2; i++) {
-			if((lista.get(i).equals(lista.get(i+1))) && ((lista.get(i+1))).equals(lista.get(i+2))){
-				posiciones.add(i);
-				posiciones.add(i+1);
-				posiciones.add(i+2);
-			}
-		}
-		return posiciones;
-	}
-
-	@Override
-	public ArrayList<Integer> busquedaColor(Colores color) {
-		ArrayList<Integer> posiciones = new ArrayList<Integer>();
-		
-		for (int i = 0; i < lista.size(); i++) {
-			if(lista.contains(color)){
-				posiciones.add(i);
-			}
-		}
-		return posiciones;
-	}
-
-	
-
-
-}
-=======
 package modelo;
 
 import java.util.ArrayList;
@@ -131,9 +61,5 @@ public class Lista implements Listable{
 		}
 		return posiciones;
 	}
-
-	
-
-
 }
->>>>>>> b26a3e92c82ea62f77df815f56fb44ad73760a40
+
--- src/modelo/Prueba.java
+++ src/modelo/Prueba.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package modelo;
 
 import java.util.ArrayList;
@@ -7,7 +6,7 @@ import java.util.LinkedList;
 public class Prueba {
 
 	public static void main(String[] args) {
-		Lista lista= new Lista();
+	/*	Lista lista= new Lista();
 		lista.addLista(Colores.amarillo);
 		lista.addLista(Colores.azul);
 		lista.addLista(Colores.azul);
@@ -17,33 +16,17 @@ public class Prueba {
 		
 		//Busqueda color
 		
-		//System.out.println(lista.busquedaColor(Colores.amarillo));
+		System.out.println(lista.busquedaColor(Colores.amarillo));
 		
 		//Borrar colores
 		
-		//System.out.println(lista.busquedaColor(Colores.amarillo));
-		//ArrayList<Integer> posiciones= new ArrayList<Integer>();
-		//posiciones.add(0);
-		//System.out.println(lista.borrarColores(posiciones));
+		ArrayList<Integer> posiciones= new ArrayList<Integer>();
+		posiciones.add(0);
+		posiciones.add(1);
+		System.out.println(lista.borrarColores(posiciones));*/
 		
 		
-		
-		
-	}
-	
-	
-}
-=======
-package modelo;
 
-public class Prueba {
-	public static void main(String[] args) {
-		Lista instancia = new Lista();
-		instancia.addLista(Colores.amarillo);
-		instancia.addLista(Colores.naranja);
-		instancia.addLista(Colores.naranja);
-		instancia.addLista(Colores.azul);
-		System.out.println(instancia.busquedaColor(Colores.naranja));
 	}
+	
 }
->>>>>>> b26a3e92c82ea62f77df815f56fb44ad73760a40
