b5f1da992b6b507183a50782796235a78a4f75e1
Linazzy <36626979+Linazzy@users.noreply.github.com>
2018-04-27 10:26:41 +0200

    vista medio

--- src/vista/desplegablePedirColor.java
+++ src/vista/desplegablePedirColor.java
@@ -19,6 +19,8 @@ import javax.swing.DefaultComboBoxModel;
 public class desplegablePedirColor extends JFrame {
 
 	private JPanel contentPane;
+	private JComboBox comboBox;
+	private JPanel panel;
 
 	/**
 	 * Launch the application.
@@ -41,28 +43,54 @@ public class desplegablePedirColor extends JFrame {
 	 */
 	public desplegablePedirColor() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 229, 208);
+		setBounds(100, 100, 397, 337);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(255, 255, 153));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel.setBounds(10, 11, 196, 58);
+		panel.setBounds(37, 13, 290, 58);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		panel.setBackground(new Color(153, 204, 255));
 		
-		JComboBox comboBox = new JComboBox(); 
+		comboBox = new JComboBox(); 
 		comboBox.setModel(new DefaultComboBoxModel(new Constantes().colores));
 		comboBox.setName("");
 		comboBox.setBackground(new Color(245, 255, 250));
 		comboBox.setForeground(new Color(0, 0, 102));
 		comboBox.setFont(new Font("Give You Glory", Font.BOLD | Font.ITALIC, 20));
 		comboBox.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		comboBox.setBounds(10, 11, 175, 36);
+		comboBox.setBounds(10, 11, 268, 36);
 		panel.add(comboBox);
 	}
+
+	public JPanel getContentPane() {
+		return contentPane;
+	}
+
+	public void setContentPane(JPanel contentPane) {
+		this.contentPane = contentPane;
+	}
+
+	public JComboBox getComboBox() {
+		return comboBox;
+	}
+
+	public void setComboBox(JComboBox comboBox) {
+		this.comboBox = comboBox;
+	}
+
+	public JPanel getPanel() {
+		return panel;
+	}
+
+	public void setPanel(JPanel panel) {
+		this.panel = panel;
+	}
+	
+	
 }
